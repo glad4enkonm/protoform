2b9664a387054c0087027a4d69d09a6ee10cfd00
Nikolai Gladchenko <ngl@kuk-is.de>
2019-11-18 16:54:53 +0100

    solution created

---
+++ Program.fs
@@ -0,0 +1,23 @@
+﻿open System
+open FParsec
+
+type Info = { syntax: string }
+let str s = pstring s
+let betweenStrings s1 s2 p = str s1 >>. p .>> str s2
+
+[<EntryPoint>]
+let main argv =
+    printfn "Hello World from F#!"
+
+    let test p str =
+        match run p str with
+        | Success(result, _, _)   -> printfn "Success: %A" result
+        | Failure(errorMsg, _, _) -> printfn "Failure: %s" errorMsg
+    
+    test pfloat "1.25"
+    test pfloat "1.25E 2"
+    
+    //test pcomment "// Interface exported by the server."
+    test pcomment "syntax = \"proto3\";"
+
+    0 // return an integer exit code
